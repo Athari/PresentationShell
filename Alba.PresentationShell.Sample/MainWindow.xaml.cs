4d4ca0715b638957efd38869843d2215fea1d281
Athari <athari.p@gmail.com>
2013-10-03 22:21:01 +0400

    Moved WrapView into Alba.Framework, fixed WrapView

--- Alba.PresentationShell.Sample/MainWindow.xaml.cs
+++ Alba.PresentationShell.Sample/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media;
@@ -46,43 +45,6 @@ namespace Alba.PresentationShell.Sample
             var item = (ShellItem)((FrameworkElement)sender).DataContext;
             if (e.ClickCount == 2)
                 item.InvokeDefault();
-        }
-    }
-
-    public class WrapView : ViewBase
-    {
-        public static readonly DependencyProperty ItemContainerStyleProperty = ItemsControl.ItemContainerStyleProperty.AddOwner(typeof(WrapView));
-        public static readonly DependencyProperty ItemTemplateProperty = ItemsControl.ItemTemplateProperty.AddOwner(typeof(WrapView));
-        public static readonly DependencyProperty ItemWidthProperty = WrapPanel.ItemWidthProperty.AddOwner(typeof(WrapView));
-        public static readonly DependencyProperty ItemHeightProperty = WrapPanel.ItemHeightProperty.AddOwner(typeof(WrapView));
-
-        public Style ItemContainerStyle
-        {
-            get { return (Style)GetValue(ItemContainerStyleProperty); }
-            set { SetValue(ItemContainerStyleProperty, value); }
-        }
-
-        public DataTemplate ItemTemplate
-        {
-            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
-            set { SetValue(ItemTemplateProperty, value); }
-        }
-
-        public double ItemWidth
-        {
-            get { return (double)GetValue(ItemWidthProperty); }
-            set { SetValue(ItemWidthProperty, value); }
-        }
-
-        public double ItemHeight
-        {
-            get { return (double)GetValue(ItemHeightProperty); }
-            set { SetValue(ItemHeightProperty, value); }
-        }
-
-        protected override object DefaultStyleKey
-        {
-            get { return new ComponentResourceKey(GetType(), "styWrapView"); }
         }
     }
 }
